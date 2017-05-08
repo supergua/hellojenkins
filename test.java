d4b2cec729a7f82eb3cc1de63f8e350ff63c8d9a
supergua <blhekai@outlook.com>
2017-05-08 16:45:57 +0800

    change 2 times

--- test.java
+++ test.java
@@ -2,6 +2,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 	System.out.println("change 1 time!");
+	System.out.println("change 2 times!");
     }
 }
 
